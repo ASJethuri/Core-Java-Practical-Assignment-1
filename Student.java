c1ade0e594ad9feeca0cbdd3b771330c2b4f16ad
ASJETHURI_30 <asjethuri2430@gmail.com>
2026-08-17 14:25:17 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,20 @@
+public class Student {
+    public static void main(String[] args) {
+
+        // Student details
+        String studentName = "Amit";
+        int rollNumber = 101;
+
+        // Marks in three subjects
+        int javaMarks = 85;
+        int mathematicsMarks = 90;
+        int dataAnalyticsMarks = 88;
+
+        // Display student details
+        System.out.println("Student Name: " + studentName);
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("Java Marks: " + javaMarks);
+        System.out.println("Mathematics Marks: " + mathematicsMarks);
+        System.out.println("Data Analytics Marks: " + dataAnalyticsMarks);
+    }
+}
